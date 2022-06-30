50b7337793c625cea0f670cdf09a3e940587384c
Daniel <ajayitimmy45@gmail.com>
2022-06-30 06:47:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 
 char str[] = "\"Programming is like building a multilingual puzzle";
 
 puts(str);
 
-return 0;
+return (0);
 }
