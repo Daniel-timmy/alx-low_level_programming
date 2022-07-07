3594b97d91075bfe4682dffb90932db1c944888d
Daniel <ajayitimmy45@gmail.com>
2022-07-07 08:11:31 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,4 +5,18 @@
  * @n: size of the square.
  * Return: no return.
  */
+void print_square(int n)
+{
+	int i, j;
 
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar(35);
+		}
+		if (i != n - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
