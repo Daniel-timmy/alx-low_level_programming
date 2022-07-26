4f7502d4902ca189d6b96e4a8b6d6433effe5a3f
Daniel <ajayitimmy45@gmail.com>
2022-07-26 04:54:07 -0700

    2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size * sizeof(char));
-	if (ptr == NUll)
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2
+ * dimensional array of integers.
+ *
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: returns a pointer to the 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (height < 1 || width < 1)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int *));
+
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+
+		if (arr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j];
+	}
+
+	return (arr);
+}
